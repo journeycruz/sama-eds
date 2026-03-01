390bf18bd5dc5e00cb958097d91053b316be0876
Journey Cruz <journey.cruz@gmail.com>
2026-03-01 01:53:35 -0600

    fix: polish footer styling and enforce sticky page footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,5 +16,16 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  const content = footer.querySelector('.default-content-wrapper');
+  const links = content?.querySelector('ul');
+
+  const paragraphs = content ? [...content.querySelectorAll(':scope > p')] : [];
+  if (paragraphs[0]) paragraphs[0].classList.add('footer-title');
+  if (paragraphs[1]) paragraphs[1].classList.add('footer-disclaimer');
+  if (paragraphs[2]) paragraphs[2].classList.add('footer-credit');
+  if (paragraphs[3]) paragraphs[3].classList.add('footer-updated');
+
+  if (links) links.classList.add('footer-links');
+
   block.append(footer);
 }
